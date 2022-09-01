8bde5ee2d48a7d3ab2a7a53905af5fa4e837d607
Dmitry <djigirdds@gmail.com>
2022-09-01 16:19:34 +0300

    add cache for movie parser

--- app/Http/Controllers/API/ParserMovieController.php
+++ app/Http/Controllers/API/ParserMovieController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Service\MovieService;
+use Illuminate\Support\Facades\Cache;
 
 class ParserMovieController extends Controller
 {
@@ -19,7 +20,12 @@ class ParserMovieController extends Controller
 
     public function index()
     {
-        $movies = $this->movieService->getNewMoviesFromHDrezka();
+//        $movies = $this->movieService->getNewMoviesFromHDrezka();
+        /* TODO добавить больше времени для кеша */
+        $movies = Cache::remember('new_movies', now()->addMinutes(1), function () {
+            return $this->movieService->getNewMoviesFromHDrezka();
+        });
+
         return response()->json($movies);
     }
 
